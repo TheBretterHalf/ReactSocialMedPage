e7abe339c5e35e9b50b331afefce9272eb120444
Brett Stainken <brettstainken.student@careerdevs.com>
2019-02-06 18:00:04 +0000

    Axios Requests

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-import Profile from "./component/Profile"
+import axios from 'axios';
 
 class Profile extends Component {
+    state = {
+        artistname: null,
+        songname: null,
+        email: null
+    }
+    
+    handleClick=()=>{
+        console.log(this.state.firstname)
+        axios.get("https://api.lyrics.ovh/v1/coldplay/yellow")
+        .then(response=>{
+            console.log(response.data)
+        })
+    }
+    
+    setArtisttName = e =>{
+        //console.log(e.target.value)
+        this.setState({
+            artistname: e.target.value
+        })
+    }
+    this.setSongName = e =>{
+        this.setState({
+            songname: e.target.value
+        })
+    }
+    
     render(){
         return(
             <div>
             <p>My App</p>
+            <input onChange={this.setArtistName} placeholder="Artist Name" />
+            <input onChange={this.setSongName} placeholder="Song Name"/>
+            <button onClick={this.handleClick}>Submit</button>
             </div>
             )
     }
