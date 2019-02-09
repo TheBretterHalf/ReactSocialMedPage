f804c1346eed3158256f7ecbe1565303b3e7fb8e
Brett Stainken <brettstainken.student@careerdevs.com>
2019-02-09 17:02:54 +0000

    Redux

---
+++ src/component/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Profile from "./component/Profile"
+
+class Profile extends Component {
+    render(){
+        return(
+            <div>
+            <p>My App</p>
+            </div>
+            )
+    }
+}
+
+export default Profile;
